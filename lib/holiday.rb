2c2ddec631a4e595b6e84cf539c3c8b81049efdf
Domenico Cascone <dcascone84@gmail.com>
2020-03-14 22:00:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supply|
-      puts "  #{event.to_s.split("_").map.each {|x| x.capitalize}.join(" ")}: #{supply.join(" ")}"
+      puts "  #{event.to_s.split("_").map.each {|x| x.capitalize}.join(" ")}: #{supply.join(", ")}"
       #binding.pry
     end
   end
